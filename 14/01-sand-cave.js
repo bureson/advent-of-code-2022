a7cf82188a195d05086be1d1cf154c8e91ea3cb7
Ondrej Bures <ondrej@triggerz.com>
2022-12-14 14:52:18 +0100

    Day 14

---
+++ 14/01-sand-cave.js
@@ -0,0 +1,81 @@
+const R = require('ramda');
+
+const { readingFile } = require('../helper');
+
+const sandCave = async () => {
+  const data = await readingFile('./input.txt');
+  const lines = data.split('\r\n').filter(x => x);
+
+  const rockMap = {};
+
+  const xList = lines.reduce((list, line) => {
+    const lineXList = line.split(' -> ').map(coord => Number(coord.split(',')[0]));
+    return list.concat(lineXList);
+  }, []);
+
+  const yList = lines.reduce((list, line) => {
+    const lineXList = line.split(' -> ').map(coord => Number(coord.split(',')[1]));
+    return list.concat(lineXList);
+  }, []);
+
+  const minX = Math.min(...xList);
+  const maxX = Math.max(...xList);
+  const minY = 0; // Math.min(...yList);
+  const maxY = Math.max(...yList);
+
+  for (let y = minY; y <= maxY; y++) {
+    if (!rockMap[y]) rockMap[y] = {};
+    for (let x = minX; x <= maxX; x++) {
+      rockMap[y][x] = '.';
+    }
+  }
+
+  for (const line of lines) {
+    const coordPairList = line.split(' -> ');
+    for (let i = 0; i < (coordPairList.length - 1); i++) {
+      const [startX, startY] = coordPairList[i].split(',').map(Number);
+      const [endX, endY] = coordPairList[i + 1].split(',').map(Number);
+      for (let y = Math.min(startY, endY); y <= Math.max(startY, endY); y++) {
+        for (let x = Math.min(startX, endX); x <= Math.max(startX, endX); x++) {
+          rockMap[y][x] = '#';
+        }
+      }
+    }
+  }
+
+  let running = true;
+  let sandCount = 0;
+
+  while (running) {
+    const sand = { x: 500, y: 0 };
+    while (running) {
+      if (sand.y + 1 > maxY || sand.x - 1 < minX || sand.x + 1 > maxX) {
+        running = false;
+        break;
+      }
+      if (rockMap[sand.y + 1][sand.x] === '.') {
+        sand.y += 1;
+        continue;
+      }
+      if (['o', '#'].includes(rockMap[sand.y + 1][sand.x])) {
+        if (rockMap[sand.y + 1][sand.x - 1] === '.') {
+          sand.y += 1;
+          sand.x -= 1;
+        } else if (rockMap[sand.y + 1][sand.x + 1] === '.') {
+          sand.y += 1;
+          sand.x += 1;
+        } else {
+          sandCount++;
+          rockMap[sand.y][sand.x] = 'o';
+          break;
+        }
+      }
+    }
+  }
+
+  console.log(Object.values(rockMap).map(line => Object.values(line).join('')).join('\n'));
+  console.log({ sandCount });
+
+};
+
+sandCave();
